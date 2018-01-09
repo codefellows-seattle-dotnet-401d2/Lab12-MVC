9f6e9bf66960832cc664ee4a9c2a5cd79c1a756b
QueensKid <andres.ibarra0716@gmail.com>
2018-01-09 12:16:45 -0800

    Initial Setup with all folders and classes created

---
+++ MVC/MVC/Models/Wine.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MVC.Models
+{
+    public class Wine
+    {
+        public static List<Wine> GetWineList()
+        {
+            List<Wine> myWine = new List<Wine>();
+            string path = Environment.CurrentDirectory;
+            //For testing, make sure the path is pointing to the root.
+            string newPath = Path.GetFullPath(Path.Combine(path, @"..\..\..\wine.csv"));
+            using (StreamReader reader = new StreamReader(newPath))
+            {
+                int counter = 0;
+                string line;
+
+                //only grab the top 1000 wines. 
+                while (((line = reader.ReadLine()) != null) && counter < 1001)
+                {
+                    Regex parser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+
+                    //Separating columns to array
+                    string[] wineList = parser.Split(line);
+
+                    //Add new wine object to the list. 
+                    myWine.Add(new Wine
+                    {
+                        ID = wineList[0],
+                        Country = wineList[1],
+                        Description = wineList[2],
+                        Designation = wineList[3],
+                        Points = wineList[4],
+                        Price = wineList[5],
+                        Region_1 = wineList[6],
+                        Region_2 = wineList[7],
+                        Variety = wineList[8],
+                        Winery = wineList[9]
+
+                    });
+
+                    counter++;
+
+                }
+            }
+
+            return myWine;
+        }
+
+    }
+}
