c855eec23aab50046d63dc6b706773947369f6e2
Ariel R. Pedraza <pedrazaa2@gmail.com>
2018-01-09 22:56:31 -0800

    modified code for lab requirements, but wine list is not populating

--- WineWebApp/WineWebApp/Controllers/HomeController.cs
+++ WineWebApp/WineWebApp/Controllers/HomeController.cs
@@ -15,34 +15,22 @@ namespace WineWebApp.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            Wine test1 = new Wine();
-            test1.testList.Add(test1.TestMethod());
-            test1.testList.Add(test1.TestMethod());
-            test1.testList.Add(test1.TestMethod());
-            Wine test2 = new Wine();
-            test2.testList.Add(test2.TestMethod());
-            test2.testList.Add(test2.TestMethod());
-            test2.testList.Add(test2.TestMethod());
-            Wine test3 = new Wine();
-            test3.testList.Add(test3.TestMethod());
-            test3.testList.Add(test3.TestMethod());
-            test3.testList.Add(test3.TestMethod());
-            List<Wine> testWineList = new List<Wine> { test1, test2, test3 };
-
-            ViewData["Name"] = "Ariel";
-
-            return View(test1);
+            return View();
         }
 
         [HttpPost]
-        public IActionResult Index(Wine w)
+        public IActionResult Index(string name, string price, string points)
         {
-            return RedirectToAction("TestView", new {w});
+            List<Wine> wineList = Wine.GetWineList();
+            IEnumerable<Wine> filteredWineList = wineList.Where(w => w.Price == price && w.Points == points);
+
+            ViewData["Name"] = name;
+            return RedirectToAction("Results", new {filteredWineList});
         }
 
-        public IActionResult TestView(Wine w)
+        public IActionResult Results(IEnumerable<Wine> filteredWineList)
         {
-            return View(w);
+            return View(filteredWineList);
         }
     }
 }
