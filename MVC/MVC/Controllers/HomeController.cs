5c266e9fef000b4f285bdaa74e00bda59219553d
jeffxmartinez <jeffxmartinez@gmail.com>
2018-01-09 22:05:45 -0800

    Added file structure

---
+++ MVC/MVC/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Index() // add params for Wine app
+        {
+            return View();
+        }
+
+        public IActionResult Results() // return information!
+        {
+            return View();
+        }
+    }
+}
