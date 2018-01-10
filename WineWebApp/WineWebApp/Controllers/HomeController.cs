9e035e297ea252b77ce44f6429c3fff2257cab4d
Ariel R. Pedraza <pedrazaa2@gmail.com>
2018-01-10 00:01:34 -0800

    Wine list now displays

--- WineWebApp/WineWebApp/Controllers/HomeController.cs
+++ WineWebApp/WineWebApp/Controllers/HomeController.cs
@@ -21,15 +21,14 @@ namespace WineWebApp.Controllers
         [HttpPost]
         public IActionResult Index(string name, string price, string points)
         {
-            List<Wine> wineList = Wine.GetWineList();
-            IEnumerable<Wine> filteredWineList = wineList.Where(w => w.Price == price && w.Points == points);
-
-            ViewData["Name"] = name;
-            return RedirectToAction("Results", new {filteredWineList});
+            return RedirectToAction("Results", new { name, price, points });
         }
 
-        public IActionResult Results(IEnumerable<Wine> filteredWineList)
+        public IActionResult Results(string name, string price, string points)
         {
+            List<Wine> wineList = Wine.GetWineList();
+            IEnumerable<Wine> filteredWineList = wineList.Where(w => w.Price == price && w.Points == points);
+            ViewData["Name"] = name;
             return View(filteredWineList);
         }
     }
