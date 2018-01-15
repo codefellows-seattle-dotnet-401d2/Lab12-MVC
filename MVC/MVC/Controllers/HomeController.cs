a8334f211a2fa1d7050e33ed0e6121cc30331f68
jeffxmartinez <jeffxmartinez@gmail.com>
2018-01-14 17:03:22 -0800

    functional wine list

--- MVC/MVC/Controllers/HomeController.cs
+++ MVC/MVC/Controllers/HomeController.cs
@@ -1,29 +1,38 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace MVC.Controllers
 {
     public class HomeController : Controller
     {
         // GET: /<controller>/
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
-        }
-        
-        public IActionResult Index() // add params for Wine app
-        {
-            return View();
+            List<Wine> wineList = Wine.GetWineList();
+            IEnumerable<Wine> WineList = wineList.Select(w => w);
+            return View(WineList.Skip(1));
         }
 
-        public IActionResult Results() // return information!
+        [HttpPost]
+        public IActionResult Index(string name, string price, string points)
         {
-            return View();
+            return RedirectToAction("Results", new { name, price, points });
+        }
+
+        public IActionResult Results(string name, string price, string points)
+        {
+            List<Wine> wineList = Wine.GetWineList();
+            IEnumerable<Wine> filteredWineList = wineList.Where(w => w.Price == price && w.Points == points);
+            ViewData["Name"] = name;
+            return View(filteredWineList);
         }
     }
 }
